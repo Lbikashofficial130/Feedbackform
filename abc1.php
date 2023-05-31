3ec99ff9dd162f03c0b502c32e6471f0299fcc58
Lbikashofficial130 <khanalbk18@gmail.com>
2023-05-31 07:24:25 +0545

    Changes all

---
+++ abc1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+
+<form action = "abc.php" method = "Post">
+<label> Number : </label>
+<input type="number" name="number" id="name" placeholder="What is Your Number?">
+<form action ="abc.php" method = "Post">
+<input type="submit" value="Submit" name="Submit" id="Submit">
+
+</body>
+</html>
